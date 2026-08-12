014ab9295be546267b4950e6a7b7c8ae0924a635
qamamatikinca-ux <qamamatikinca@icloud.com>
2026-08-12 14:45:39 +0200

    Load universal LoadLink UI after existing design CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Suspense } from "react";
 import "./globals.css";
 import "./loadlink-logistics-modal.css";
 import "./loadlink-v2714-design.css";
+import "./loadlink-universal-ui.css";
 import GlobalLoading from "@/components/GlobalLoading";
 import ChatLauncher from "@/components/ChatLauncher";
 import SwipeDotsEnhancer from "@/components/SwipeDotsEnhancer";
